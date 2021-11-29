2b807601dd8b4a63e6ba1cf3f41963beb8646578
Lizardo <lizardo@utp.edu.co>
2021-11-29 18:39:21 -0500

    Initial Commit

---
+++ Ejercicio1.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+import uvicorn
+
+db = []
+
+
+class City(BaseModel):
+    name: str
+    timezone: str
+
+
+app = FastAPI()
+
+
+@app.get("/cities")
+async def get_cities():
+    return db
+
+
+@app.post("/cities/", response_model=City)
+async def create_city(newcity=City):
+    name = newcity.name
+    timezone = newcity.timezone
+
+    db.append(newcity.dict())
+    city = {"name": name, "timezone": timezone}
+    return city
+
+
+if __name__ == "__main__":
+    uvicorn.run("Ejercicio1:app")
